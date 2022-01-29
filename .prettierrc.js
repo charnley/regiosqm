b4c9186d6ea464b45d745be485958db602b90d0d
Jimmy Charnley Kromann <jimmy@charnley.dk>
2022-01-29 09:43:44 +0100

    init js

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    printWidth: 120,
+    singleQuote: true,
+    trailingComma: 'all',
+    semi: false,
+    tabWidth: 4,
+    bracketSpacing: false,
+    bracketSameLine: true,
+}
