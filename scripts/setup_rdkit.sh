2096898b88c0a2f399b371f97b52b7fe6ba085b4
Jimmy Charnley Kromann <jimmy@charnley.dk>
2022-01-30 21:16:13 +0100

    setup rdkit

---
+++ scripts/setup_rdkit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd public
+
+# VERSION=
+
+wget https://unpkg.com/@rdkit/rdkit/Code/MinimalLib/dist/RDKit_minimal.js
+wget https://unpkg.com/@rdkit/rdkit/Code/MinimalLib/dist/RDKit_minimal.wasm
